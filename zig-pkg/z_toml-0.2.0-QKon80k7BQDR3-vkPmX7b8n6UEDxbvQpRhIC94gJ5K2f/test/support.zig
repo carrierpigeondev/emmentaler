7b5fcff3e368b74840097f7b520995eda1b0f650
carrierpigeondev <carrierpigeondev@noreply.codeberg.org>
2026-05-21 03:50:03 -0500

    accidentally including zig-pkg my bad

--- zig-pkg/z_toml-0.2.0-QKon80k7BQDR3-vkPmX7b8n6UEDxbvQpRhIC94gJ5K2f/test/support.zig
+++
@@ -1,5 +0,0 @@
-const std = @import("std");
-
-pub fn readTestFile(gpa: std.mem.Allocator, path: []const u8) ![]u8 {
-    return std.Io.Dir.cwd().readFileAlloc(std.testing.io, path, gpa, .limited(std.math.maxInt(usize)));
-}
